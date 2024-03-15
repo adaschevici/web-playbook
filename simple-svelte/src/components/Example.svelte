cb6cec958309ec116366a0a1753a64e66e2a54f2
Artur Das <a.daschevici@gmail.com>
2024-03-15 09:46:06 +0100

    Add simple skeleton to test astro and tailwindcss with svelte

---
+++ simple-svelte/src/components/Example.svelte
@@ -0,0 +1,14 @@
+<script>
+  let name = "Astro + Svelte";
+</script>
+
+
+<style>
+</style>
+
+<h1>Hello {name}!</h1>
+<div class="p-4 max-w-sm mx-auto bg-white rounded-xl shadow-md">
+  <h1 class="text-xl font-semibold text-gray-900">Hello, Tailwind!</h1>
+  <p class="mt-2 text-gray-100">You're now using Tailwind CSS in Astro + Svelte.</p>
+</div>
+
