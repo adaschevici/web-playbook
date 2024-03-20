c5c33c3be45227ae6fc7244f878bd51a13d2c40a
Artur Das <a.daschevici@gmail.com>
2024-03-20 19:53:30 +0100

    Fix up styles a bit but still hacking blindly for now

--- state-management/src/components/CartFlyout.svelte
+++ state-management/src/components/CartFlyout.svelte
@@ -4,16 +4,50 @@
 
 {#if $isCartOpen}
   {#if Object.values($cartItems).length}
-    <aside>
-      {#each Object.values($cartItems) as cartItem}
-      <li>
-        <img src={cartItem.imageSrc} alt={cartItem.name} />
-        <h3>{cartItem.name}</h3>
-        <p>Quantity: {cartItem.quantity}</p>
-      </li>
-      {/each}
+    <aside class="container">
+      <ul class="list">
+        {#each Object.values($cartItems) as cartItem}
+        <li class="listItem">
+          <img src={cartItem.imageSrc} alt={cartItem.name} />
+          <h3>{cartItem.name}</h3>
+          <p>Quantity: {cartItem.quantity}</p>
+        </li>
+        {/each}
+      </ul>
     </aside>
   {:else}
     <p>Your cart is empty!</p>
   {/if}
 {/if}
+
+<style>
+  .container {
+    position: fixed;
+    right: 0;
+    top: var(--nav-height);
+    height: 100vh;
+    background: var(--color-bg-2);
+    padding-inline: 2rem;
+    min-width: min(90vw, 300px);
+    border-left: 3px solid var(--color-bg-3);
+  }
+
+  .list {
+    list-style: none;
+    padding: 0;
+  }
+
+  .listItem {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+  }
+
+  .listItem * {
+    margin-block: 0.3rem;
+  }
+
+  .listItemImg {
+    width: 4rem;
+  }
+</style>
