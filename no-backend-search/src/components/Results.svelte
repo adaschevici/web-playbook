20896f9ddbaeb86da55b10e6557e9f8f69ef2676
Artur Das <a.daschevici@gmail.com>
2024-03-26 20:38:57 +0100

    Add svelte component for search result items

--- no-backend-search/src/components/Results.svelte
+++ no-backend-search/src/components/Results.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
   import { searchResults } from '../stores/search.ts';
-  // import { Card } from "./Card.astro";
-  // let results = searchResults || [];
+  import Card from './Card.svelte';
 </script>
 
 {#if $searchResults.length === 0}
   {$searchResults}
   <p>No results found</p>
 {:else}
+  {#each $searchResults as post}
+    <Card href={post.frontmatter.slug} title={post.frontmatter.title} body={post.Content} />
+  {/each}
   <p>Found {$searchResults.length} results</p>
 {/if}
-<!-- {#each results as post} -->
-<!--   <Card -->
-<!--     href={post.frontmatter.slug} -->
-<!--     title={post.frontmatter.title} -->
-<!--     body={post.Content} -->
-<!--   /> -->
-<!-- {/each} -->
