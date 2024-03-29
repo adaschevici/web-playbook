31ba5033fae7e15aac5ac887318eb7e88504842e
Artur Das <a.daschevici@gmail.com>
2024-03-30 00:43:15 +0100

    Update results card to use description instead of content as content is an astro thing

--- no-backend-search/src/components/Results.svelte
+++ no-backend-search/src/components/Results.svelte
@@ -6,7 +6,11 @@
 {#if $searchResults.length !== 0}
   <div class="absolute flex w-full flex-col gap-6">
     {#each $searchResults as post}
-      <Card href={post.frontmatter.slug} title={post.frontmatter.title} body={post.Content} />
+      <Card
+        href={post.frontmatter.slug}
+        title={post.frontmatter.title}
+        body={post.frontmatter.description}
+      />
     {/each}
     <p>Found {$searchResults.length} results</p>
   </div>
