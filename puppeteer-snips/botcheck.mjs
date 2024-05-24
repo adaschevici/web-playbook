d72a031b0cfbbf772116ea86f5a59da037f04a29
Artur Das <a.daschevici@gmail.com>
2024-05-24 12:43:02 +0200

    Add a new url for checking bot

--- puppeteer-snips/botcheck.mjs
+++ puppeteer-snips/botcheck.mjs
@@ -4,11 +4,19 @@ import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 puppeteer.use(StealthPlugin());
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 // puppeteer usage as normal
 puppeteer.launch({ headless: true }).then(async (browser) => {
   console.log("Running tests..");
   const page = await browser.newPage();
-  await page.goto("https://bot.sannysoft.com");
+  await page.goto("https://nowsecure.nl", {
+    waitUntil: "networkidle2",
+  });
+  // await page.goto("https://bot.sannysoft.com");
+  // await sleep(50000);
   await page.screenshot({
     path: "./screenshots/testresult.png",
     fullPage: true,
