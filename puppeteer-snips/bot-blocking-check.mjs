fee409726291eb55778fdf590e3e860ab209f994
Artur Das <a.daschevici@gmail.com>
2024-06-04 16:25:22 +0200

    Add bot checked site to test how it works with puppeteer and it seems to be

---
+++ puppeteer-snips/bot-blocking-check.mjs
@@ -0,0 +1,44 @@
+import puppeteer from "puppeteer-extra";
+
+// add stealth plugin and use defaults (all evasion techniques)
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+puppeteer.use(StealthPlugin());
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// puppeteer usage as normal
+puppeteer
+  .launch({ headless: false, defaultViewport: null })
+  .then(async (browser) => {
+    console.log("Running tests..");
+    const page = await browser.newPage();
+    await page.goto("https://www.g2.com/products/jira/reviews/", {
+      waitUntil: "networkidle0",
+    });
+    // await page.goto("https://bot.sannysoft.com");
+    // await sleep(50000);
+    const internalIframe = await page.$("div#turnstile-wrapper iframe");
+    console.log(internalIframe);
+    const frame = await internalIframe.contentFrame();
+    console.log(frame);
+    await frame.waitForSelector(
+      "div#challenge-stage label.cb-lb input[type='checkbox']",
+    );
+    const button = await frame.$(
+      "div#challenge-stage label.cb-lb input[type='checkbox']",
+    );
+    await sleep(5000);
+    await button.click();
+    await sleep(25000);
+    console.log(button);
+    // await button.click();
+    // console.log(await button.evaluate((node) => node.textContent));
+    await page.screenshot({
+      path: "./screenshots/testresult.png",
+      fullPage: true,
+    });
+    await browser.close();
+    console.log(`All done, check the screenshot. ✨`);
+  });
