17b27aedb361a375e0e98ea970f435a632e4c737
Artur Das <a.daschevici@gmail.com>
2024-06-20 19:03:39 +0200

    Add an extra font and apply it to the UI

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
   return (
@@ -15,7 +16,7 @@ export default function Page() {
 	  <h1 className="text-3xl font-bold text-gray-800 md:text-5xl">
 	    Welcome to Acme
 	  </h1>
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
