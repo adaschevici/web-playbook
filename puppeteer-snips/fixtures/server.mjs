df70dfac0edfa104ab344a0b7b825e3fef541a95
Artur Das <a.daschevici@gmail.com>
2024-08-05 22:46:26 +0200

    Fix file serving with html

--- puppeteer-snips/fixtures/server.mjs
+++ puppeteer-snips/fixtures/server.mjs
@@ -1,7 +1,11 @@
 import http from "http";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
+// Manually define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Define the port to run the server on
 const port = 3000;
 
@@ -9,7 +13,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   // Serve the index.html file
   if (req.method === "GET" && req.url === "/") {
-    const filePath = path.join(__dirname, "index.html");
+    const filePath = path.join(__dirname, "shadow-dom.html");
 
     fs.readFile(filePath, (err, data) => {
       if (err) {
